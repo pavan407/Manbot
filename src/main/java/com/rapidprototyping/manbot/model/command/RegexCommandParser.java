63e9ec293d8703b6781c7fd07aa2c899f7547006
DylanReedy <dylan.p.reedy@gmail.com>
2018-01-28 16:45:26 -0800

    created RegexCommandParser

---
+++ src/main/java/com/rapidprototyping/manbot/model/command/RegexCommandParser.java
@@ -0,0 +1,47 @@
+package com.rapidprototyping.manbot.model.command;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexCommandParser implements CommandParser {
+
+    Pattern pattern = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
+    /**
+     *
+     * @param input must have at least one non-whitespace character
+     * @return determines whether or not the lead character of the input is a '!' character or not
+     */
+    @Override
+    public boolean isCommandCandidate(String input) {
+        if(input==null || input.trim().length() == 0){
+            return false;
+        }else{
+            return input.trim().charAt(0) == '!';
+        }
+    }
+
+    @Override
+    public Command parseCommand(String input) {
+
+        ArrayList<String> tokenizedMessage = new ArrayList<>();
+        Matcher regexMatcher;
+        regexMatcher = pattern.matcher(input);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group(1) != null) {
+                // DQ_TOKEN
+                tokenizedMessage.add(regexMatcher.group(1));
+            } else if (regexMatcher.group(2) != null) {
+                // SQ_TOKEN
+                tokenizedMessage.add(regexMatcher.group(2));
+            } else {
+               // SIMPLE_TOKEN
+               tokenizedMessage.add(regexMatcher.group());
+            }
+        }
+        String name = tokenizedMessage.get(0);
+        tokenizedMessage.remove(0);
+        ArrayList<String> args = tokenizedMessage;
+        return new Command(name,tokenizedMessage);
+    }
+}
